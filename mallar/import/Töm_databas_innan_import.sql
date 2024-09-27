840fec93b906a45641fc3899af010e256422a745
magnus.sandberg <magnussandberg.sverige@gmail.com>
2024-09-27 13:22:32 +0200

    inför SM 2024

--- mallar/import/Töm_databas_innan_import.sql
+++ mallar/import/Töm_databas_innan_import.sql
@@ -7,12 +7,12 @@ TO DISK = 'C:\temp\Voltige_innanrensning\WebApplication1.Classes.VaultingContext
       NAME = 'Full Backup of WebApplication1.Classes.VaultingContext'; 
 
 BEGIN TRANSACTION; 
---truncate table vaulterOrders
---Delete from Vaulters
---Delete from HorseOrders
---Delete from TeamLists
---Delete from Teams
---Delete from Horses
---Delete from Lungers
---Delete from Clubs
+truncate table vaulterOrders
+Delete from Vaulters
+Delete from HorseOrders
+Delete from TeamLists
+Delete from Teams
+Delete from Horses
+Delete from Lungers
+Delete from Clubs
 Commit
